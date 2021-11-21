6cbb25d0e23d22d1f982cabe5a138ee4ee547c2c
Long Ptit <longptit.06@gmail.com>
2021-11-21 10:28:58 +0700

    init base code for nurse

---
+++ src/main/java/com/example/ltw_longptit/model/Yta.java
@@ -0,0 +1,26 @@
+package com.example.ltw_longptit.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table
+public class Yta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column
+    private int ytaThamNien;
+    @Column
+    private String ytaCMT;
+    @Column
+    private String ytaTen;
+    @Column
+    private String ytaSDT;
+    @Column
+    private String ytaDiaChi;
+    @Column
+    private String ytaNgaySinh;
+}
